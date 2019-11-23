87c24cf80e08241c16d992ff98c3dda331b2c5e3
matiascrespo <40859037+matiascrespo@users.noreply.github.com>
2019-11-23 17:09:07 -0300

    Tp 4

---
+++ BarriosCrespo.Matias.2A.TP4/Entidades/Correo.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Entidades
+{
+    public class Correo : IMostrar<List<Paquete>>
+    {
+        private List<Thread> mockPaquetes;
+        private List<Paquete> paquetes;
+
+        // public  List<Paquete> Paquetes { get; set; }
+
+        public List<Paquete> Paquetes
+        {
+            get
+            {
+                return this.paquetes;
+            }
+            set
+            {
+                this.paquetes = value;
+            }
+        }
+
+        public Correo()
+        {
+            this.mockPaquetes = new List<Thread>();
+            this.paquetes = new List<Paquete>();
+
+        }
+
+        public void FinEntregas()
+        {
+            foreach (Thread hilosActivos in this.mockPaquetes)
+            {
+                hilosActivos.Abort();
+            }
+
+        }
+
+        public string MostrarDatos(IMostrar<List<Paquete>> elementos)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (Paquete aux in ((Correo)elementos).paquetes)
+            {
+                sb.AppendLine(string.Format("{0} para {1} ({2})", aux.TrackingID, aux.DireccionEntrega, aux.Estado.ToString()));
+
+            }
+            return sb.ToString();
+
+
+        }
+
+        public static Correo operator +(Correo c, Paquete p)
+        {
+            //Dudas en el alcance del foreach
+            foreach (Paquete aux in c.paquetes)
+            {
+                if (aux == p)
+                {
+                    throw new TrackingIdRepetidoException("El paquete ya se encuentra en el correo");
+
+                }
+            }
+
+
+            c.paquetes.Add(p);
+            Thread hiloPaquete = new Thread(p.MockCicloDeVida);
+            c.mockPaquetes.Add(hiloPaquete);
+            hiloPaquete.Start();
+
+
+
+            return c;
+        }
+    }
+}
---
+++ BarriosCrespo.Matias.2A.TP4/Entidades/Paquete.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Entidades
+{
+    public class Paquete:IMostrar<Paquete>
+    {
+        private string direccionEntrega;
+        private EEstado estado;
+        private string trackingId;
+
+        public delegate void DelegadoEstado(object sender, EventArgs e);
+
+        public DelegadoEstado InformaEstado;
+        
+
+        public enum EEstado
+        {
+            Ingresado,
+            EnViaje,
+            Entregado
+        }
+
+        public Paquete(string direccionEntrega, string trackingID)
+        {
+            this.direccionEntrega = direccionEntrega;
+            this.trackingId = trackingID;
+        }
+
+        #region propiedades
+
+        public string DireccionEntrega
+        {
+            get
+            {
+                return this.direccionEntrega;
+            }
+
+            set
+            {
+                this.direccionEntrega = value;
+            }
+        }
+
+        public EEstado Estado
+        {
+            get
+            {
+                return this.estado;
+            }
+
+            set
+            {
+                this.estado = value;
+            }
+        }
+
+        public string TrackingID
+        {
+            get
+            {
+                return this.trackingId;
+            }
+
+            set
+            {
+                this.trackingId = value;
+            }
+        }
+
+        #endregion
+
+        #region sobrecarga operadores
+        public static bool operator==(Paquete p1, Paquete p2)
+        {
+            bool retorno = false;
+            if (p1.trackingId == p2.trackingId)
+                retorno = true;
+            return retorno;
+        }
+
+        public static bool operator !=(Paquete p1, Paquete p2)
+        {
+            return !(p1 == p2);
+        }
+
+
+        #endregion
+
+        #region sobrecarga metodos
+
+        public override string ToString()
+        {
+            return MostrarDatos(this);
+            
+        }
+        #endregion
+
+        #region metodos
+        public string MostrarDatos(IMostrar<Paquete> elemento)
+        {
+            return string.Format("{0} para {1}",((Paquete)elemento).TrackingID, ((Paquete)elemento).DireccionEntrega);
+        }
+
+        /*a. Colocar una demora de 4 segundos.
+        b. Pasar al siguiente estado.
+        c. Informar el estado a través de InformarEstado. EventArgs no tendrá ningún dato extra.
+        d. Repetir las acciones desde el punto A hasta que el estado sea Entregado.
+        e. Finalmente guardar los datos del paquete en la base de datos */
+
+        public void MockCicloDeVida()
+        {            
+                
+                while(this.estado != EEstado.Entregado)
+                {
+                
+                    Thread.Sleep(4000);
+                    if(this.estado == EEstado.Ingresado)
+                    {
+                        this.estado = EEstado.EnViaje;
+                    }
+                    else if(this.estado == EEstado.EnViaje)
+                    {
+                        this.estado = EEstado.Entregado;
+                    }
+                    this.InformaEstado(this, EventArgs.Empty);
+                }
+            
+
+        try
+        {
+                PaqueteDAO.Insertar(this);
+        
+        }
+            catch(Exception e)
+            {
+               MessageBox.Show(e.Message);
+            }
+        }
+
+        #endregion 
+
+    }
+}
