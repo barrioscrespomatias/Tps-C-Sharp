e8c334731e6db4375ba927ece72aaf10cac7148f
unknown <matiascrespo>
2020-11-29 15:34:05 -0300

    Recuperatorio TP-4

---
+++ Recuperatorios TP/TP-04/BarriosCrespo.Matias.2A.TP4/Entidades/Enumerados.cs
@@ -0,0 +1,22 @@
+﻿//Lista enumerados
+public enum EEdad
+{
+    Peques,
+    Medianos,
+    Grandes,
+    EdadIncorrecta
+}
+
+public enum EPeriodoInscripcion
+{
+    Semana,
+    Quincena,
+    Mes
+}
+
+public enum EMesIncripcion
+{
+    Diciembre,
+    Enero,
+    Febrero
+}
